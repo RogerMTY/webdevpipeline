f7ba83d774acb06503882fe2dda5d0c04830b411
RogerMTY <76199913+RogerMTY@users.noreply.github.com>
2023-02-15 14:45:07 -0600

    Add files via upload

---
+++ Act 1/Main.js
@@ -0,0 +1,14 @@
+function changeColor(newColor) {
+    var elem = document.getElementById('the-title');
+    elem.style.color = newColor;
+ }
+
+function cambiaColor(newColor) {
+    var elemento = document.getElementById('nuevo-titulo');
+    elemento.style.color = newColor;
+ }
+
+function modificarBoton() {
+    var elemento = document.getElementById('miboton');
+    elemento.innerText = "Este es mi boton, de nadie mas, no puedes tenerlo, porque es mi boton."
+}
